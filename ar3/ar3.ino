82b081cda12b5dfa8dc720154307df6e3c029cf5
Shri Abhishek Choudhary <92842067+shriabhishekk@users.noreply.github.com>
2023-09-18 01:29:05 +0530

    Add files via upload

---
+++ ar3/ar3.ino
@@ -0,0 +1,14 @@
+if serial.available()
+btdata.serial.read
+serial.data()
+serial.print()
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
