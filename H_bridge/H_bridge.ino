409f74fdff6804a73a1cd1c2a2840a1443acd543
Shri Abhishek Choudhary <92842067+shriabhishekk@users.noreply.github.com>
2023-09-25 00:25:20 +0530

    Add files via upload

---
+++ H_bridge/H_bridge.ino
@@ -0,0 +1,84 @@
+#define enb1 9
+#define left1 4
+#define left2 5
+#define right1 6
+#define right2 7
+#define enb2 10
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(enb1,OUTPUT);
+pinMode(left1,OUTPUT);
+pinMode(left2,OUTPUT);
+pinMode(right1,OUTPUT);
+pinMode(right2,OUTPUT);
+pinMode(enb2,OUTPUT);
+
+digitalWrite(left1,HIGH);
+digitalWrite(left2,HIGH);
+digitalWrite(right1,HIGH);
+digitalWrite(right2,HIGH);
+analogWrite(enb1,255);
+analogWrite(enb2,255);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+front();
+delay(10000);
+stp();
+delay(5000);
+back();
+delay(10000);
+stp();
+delay(5000);
+}
+
+ //function for left turn
+void leftturn(){
+    
+  
+}
+
+
+//function for 
+void rightturn(){
+  
+}
+void front(){
+  
+  analogWrite(enb1,n);
+  analogWrite(enb2,n);
+   }
+   digitalWrite(left1,HIGH);
+   digitalWrite(left2,LOW);
+   digitalWrite(right1,HIGH);
+   digitalWrite(right2,LOW);
+}
+
+void back(){
+
+  analogWrite(enb1,n);
+  analogWrite(enb2,n);
+  }
+  
+   digitalWrite(left1,LOW);
+   digitalWrite(left2,HIGH);
+   digitalWrite(right1,LOW);
+   digitalWrite(right2,HIGH);
+
+}
+
+void stp(){
+  analogWrite(enb1,n);
+  analogWrite(enb2,n);
+  
+  }
+   digitalWrite(left1,LOW);
+   digitalWrite(left2,LOW);
+   digitalWrite(right1,LOW);
+   digitalWrite(right2,LOW);
+   
+}
